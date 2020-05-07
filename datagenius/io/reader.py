5df62b208610f1d460b7a6479a297a229f6ae53c
Chris Larabee <christopher.larabee@mnhs.org>
2020-05-07 16:44:19 +0000

    Aded a read_excel function so that multi-sheet workbooks can be handled.

--- datagenius/io/reader.py
+++ datagenius/io/reader.py
@@ -1,5 +1,7 @@
 import csv
 
+import xlrd
+
 
 def read_csv(file_name):
     """
@@ -8,7 +10,7 @@ def read_csv(file_name):
     a complete header.
 
     Args:
-        file_name: The path to the file to read.
+        file_name: The path to the csv file to read.
 
     Returns: A list of lists, one for each row in the sheet,
         with indexes being the values from each cell in the row.
@@ -39,4 +41,28 @@ def read_sheet(sheet):
         for j in range(sheet.ncols):
             row.append(sheet.cell_value(i, j))
         result.append(row)
+    return result
+
+
+def read_excel(file_name):
+    """
+
+    Loops all sheets in an excel workbook and returns a dictionary
+    with sheet names as keys and the results of read_sheet() for
+    that corresponding sheet as the values.
+
+    Args:
+        file_name: The path to the excel file to read.
+
+    Returns: A dictionary containing sheet name keys and list of
+        list values representing the data in each sheet.
+
+    """
+    wb = xlrd.open_workbook(file_name)
+    names = wb.sheet_names()
+    result = dict()
+
+    for i in range(wb.nsheets):
+        result[names[i]] = read_sheet(wb.sheet_by_index(i))
+
     return result
