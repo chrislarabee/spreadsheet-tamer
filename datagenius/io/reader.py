633be42651345d647f9f2a1952ed4cc349f5844b
Chris Larabee <christopher.larabee@mnhs.org>
2020-05-07 19:11:16 +0000

    Added type markers for reader function parameters.

--- datagenius/io/reader.py
+++ datagenius/io/reader.py
@@ -4,7 +4,7 @@ import os
 import xlrd
 
 
-def read_csv(file_name):
+def read_csv(file_name: str):
     """
     Quickly reads a well-formatted csv file to a list of lists.
     Also, attempts to parse data as numeric and uses float
@@ -35,7 +35,7 @@ def read_csv(file_name):
     return {key: result}
 
 
-def read_excel(file_name):
+def read_excel(file_name: str):
     """
 
     Loops all sheets in an excel workbook and returns a dictionary
@@ -59,7 +59,7 @@ def read_excel(file_name):
     return result
 
 
-def read_file(file_name):
+def read_file(file_name: str):
     """
     Uses the appropriate reader function based on the extension
     of the passed file.
@@ -87,7 +87,7 @@ def read_file(file_name):
         return read_funcs[ext](file_name)
 
 
-def read_sheet(sheet):
+def read_sheet(sheet: xlrd.sheet):
     """
     Reads all rows and columns in a Microsoft excel sheet and creates
     a list of lists with all values from the spreadsheet.
