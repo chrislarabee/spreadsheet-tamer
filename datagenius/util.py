6535f6aad1150f61bd9581ce109e22ea130a665c
Chris Larabee <christopher.larabee@mnhs.org>
2020-05-12 15:27:56 +0000

    Added a simple util function that counts strings in lists and accompanying test.

--- datagenius/util.py
+++ datagenius/util.py
@@ -8,7 +8,23 @@ def non_null_count(x: list) -> int:
     Args:
         x: A list.
 
-    Returns: An integer, the cont of non-nulls in the list.
+    Returns: An integer, the count of non-nulls in the list.
 
     """
     return sum([0 if y in (None, '') else 1 for y in x])
+
+
+def true_str_count(x: list) -> int:
+    """
+    Takes a list and returns the number of values in it that are
+    strings that are not ''.
+
+    Args:
+        x: A list.
+
+    Returns: An integer, the count of non-blank strings.
+
+    """
+    return sum(
+        [1 if isinstance(y, str) and y != '' else 0 for y in x]
+    )
--- tests/util.py
+++ tests/util.py
@@ -5,3 +5,8 @@ def test_non_null_count():
     assert u.non_null_count(['', '', '']) == 0
     assert u.non_null_count([1, '', '']) == 1
     assert u.non_null_count([1, 2, 3]) == 3
+
+
+def test_true_str_count():
+    assert u.true_str_count(['', '', '']) == 0
+    assert u.true_str_count(['a', 'test', 1]) == 2
