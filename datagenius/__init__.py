7dc3591bf411403426f95afd0f390b7d809a781a
Chris Larabee <christopher.larabee@mnhs.org>
2020-06-11 22:09:59 +0000

    Added lib package and preprocess sub-module. lib will store functions that currently live under the various Genius objects.

--- datagenius/__init__.py
+++ datagenius/__init__.py
@@ -2,5 +2,6 @@ from .element import Mapping, MetaData, Rule, MatchRule
 # from .genius import (
 #     parser, Genius, Preprocess, Clean, Explore, Reformat, Supplement)
 from .io import odbc, text
+from .lib import preprocess
 
 __all__ = ['element', 'genius', 'util']
---
+++ datagenius/lib/preprocess.py
@@ -0,0 +1,35 @@
+from typing import Optional, Sequence
+
+import datagenius.element as e
+import datagenius.util as u
+
+
+def detect_header(
+        ds: e.Dataset,
+        manual_header: Optional[Sequence] = None) -> e.Dataset:
+    """
+    Takes a Dataset object and sets its column names to be the
+    values of the first row containing all true strings and removes
+    that row from the Dataset.
+
+    Args:
+        ds: A Dataset object.
+        manual_header: A Sequence of labels to use as column names.
+            Useful when your Dataset has no discernible header.
+
+    Returns: The Dataset, with the header row extracted as column
+        names.
+
+    """
+    if manual_header:
+        ds.columns = manual_header
+    else:
+        true_str_series = ds.apply(
+            lambda x: u.count_true_str(x) == len(x), axis=1
+        )
+        first_idx = next(
+            (i for i, v in true_str_series.items() if v), None)
+        if first_idx:
+            ds.columns = list(ds.iloc[first_idx])
+            return ds.drop(index=first_idx).reset_index(drop=True)
+    return ds
