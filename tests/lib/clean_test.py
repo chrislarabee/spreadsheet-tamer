e1652683d8ae11f7106b6c94a326a23ce8e28261
Chris Larabee <christopher.larabee@mnhs.org>
2020-06-20 20:51:37 +0000

    Added a test to ensure that complete_clusters handles repeating values with nulls between.

--- tests/lib/clean_test.py
+++ tests/lib/clean_test.py
@@ -4,12 +4,31 @@ from numpy import nan
 import datagenius.lib.clean as cl
 
 
-def test_de_cluster(needs_extrapolation, employees):
+def test_complete_clusters(needs_extrapolation, employees):
     df = pd.DataFrame(**needs_extrapolation)
     df, md_dict = cl.complete_clusters(df, ['department'])
     pd.testing.assert_frame_equal(df, pd.DataFrame(**employees))
     expected_metadata = pd.DataFrame([
         dict(department=2)
+    ])
+    pd.testing.assert_frame_equal(md_dict['metadata'], expected_metadata)
+
+    df = pd.DataFrame([
+        dict(a=1, b=2, c=3),
+        dict(a=nan, b=nan, c=nan),
+        dict(a=1, b=nan, c=4),
+        dict(a=nan, b=nan, c=nan),
+    ])
+    expected = pd.DataFrame([
+        dict(a=1.0, b=2.0, c=3.0),
+        dict(a=1.0, b=2.0, c=3.0),
+        dict(a=1.0, b=2.0, c=4.0),
+        dict(a=1.0, b=2.0, c=4.0),
+    ])
+    df, md_dict = cl.complete_clusters(df, ['a', 'b', 'c'])
+    pd.testing.assert_frame_equal(df, expected)
+    expected_metadata = pd.DataFrame([
+        dict(a=2, b=3, c=2)
     ])
     pd.testing.assert_frame_equal(md_dict['metadata'], expected_metadata)
 
