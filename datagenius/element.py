2627c2778deec3b84d214c51c1d6ba06b0930a5f
Chris Larabee <christopher.larabee@mnhs.org>
2020-06-22 02:59:58 +0000

    Added ZeroNumeric object to element.

--- datagenius/element.py
+++ datagenius/element.py
@@ -158,6 +158,54 @@ class CleaningGuide(Element, col.abc.Mapping, col.abc.Callable):
         return self._data.__iter__()
 
 
+class ZeroNumeric:
+    """
+    Numeric strings that need to have one or more zeros on their left
+    side.
+    """
+    def __init__(self, value):
+        """
+
+        Args:
+            value: A string that could be converted to a numeric value.
+        """
+        if not u.isnumericplus(value):
+            raise ValueError(f'ZeroNumeric must be a numeric string or '
+                             f'a numeric value. Invalid value={value}')
+        self._value = str(value)
+
+    def pad(self, length: int) -> str:
+        """
+        Returns the value of the ZeroNumeric but with additional zeros
+        on the left side to ensure the length of the ZeroNumeric is
+        equal to length.
+
+        Args:
+            length: An integer, the desired character length of the
+                padded ZeroNumeric
+
+        Returns: The value of the ZeroNumeric with as many zeros on the
+            left as are necessary for len(ZeroNumeric) == length.
+
+        """
+        return '0' * max(length - len(self), 0) + self._value
+
+    def __eq__(self, other):
+        return self._value == other
+
+    def __len__(self):
+        return len(self._value)
+
+    def __ne__(self, other):
+        return self._value != other
+
+    def __repr__(self):
+        return self._value
+
+    def __str__(self):
+        return "'" + self._value
+
+
 class Rule:
     """
     Highly flexible callable object that can store and execute
--- tests/element_test.py
+++ tests/element_test.py
@@ -48,6 +48,21 @@ class TestCleaningGuide:
             cg = e.CleaningGuide.convert('test')
 
 
+class TestZeroNumeric:
+    def test_basics(self):
+        assert e.ZeroNumeric('00123') == '00123'
+        assert e.ZeroNumeric('00123') != '123'
+        assert str(e.ZeroNumeric('00123')) == "'00123"
+
+        with pytest.raises(
+                ValueError, match='numeric value. Invalid value=test'):
+            e.ZeroNumeric('test')
+
+    def test_pad(self):
+        assert e.ZeroNumeric('123').pad(3) == '123'
+        assert e.ZeroNumeric('123').pad(6) == '000123'
+
+
 class TestRule:
     def test_init(self):
         r = e.Rule(lambda x: x + 1, 'test')
