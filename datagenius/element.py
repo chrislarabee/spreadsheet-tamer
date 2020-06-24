2b2a289099ec6f9b6c0ac6ff97e12f7d89ef86ee
Chris Larabee <christopher.larabee@mnhs.org>
2020-06-24 21:56:46 +0000

    Upgraded ZeroNumeric so that it can be operated on as if it were both a numeric value and a string.

--- datagenius/element.py
+++ datagenius/element.py
@@ -1,27 +1,48 @@
+import re
+import operator as o
+
+import pandas as pd
+
 import datagenius.util as u
 
 
 class ZeroNumeric:
-    # TODO: Make this more powerful by storing the zeros separately
-    #       and the numeric portion as a numeric value, so that
-    #       mathematical operations can be carried out on it without
-    #       sacrificing the initial zeros.
     """
     Numeric strings that need to have one or more zeros on their left
     side.
     """
+    @property
+    def zeros(self):
+        return self._zeros
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def numeric(self):
+        return self._numeric
+
     def __init__(self, value):
         """
 
         Args:
             value: A string that could be converted to a numeric value.
         """
-        if not u.isnumericplus(value):
-            raise ValueError(f'ZeroNumeric must be a numeric string or '
-                             f'a numeric value. Invalid value={value}')
-        self._value = str(value)
+        self._zeros: str = ''
+        self._numeric: (int, float, None) = None
+        if pd.isna(value):
+            self._value = value
+            self._numeric = value
+        elif u.isnumericplus(value):
+            self._value = value
+            self._zeros, self._numeric = self.split_zeros(value)
+        else:
+            raise ValueError(
+                f'ZeroNumeric must be a numeric string, numeric value, '
+                f'or nan. Invalid value={value}')
 
-    def pad(self, length: int) -> str:
+    def pad(self, length: int = None):
         """
         Returns the value of the ZeroNumeric but with additional zeros
         on the left side to ensure the length of the ZeroNumeric is
@@ -31,23 +52,113 @@ class ZeroNumeric:
             length: An integer, the desired character length of the
                 padded ZeroNumeric
 
-        Returns: The value of the ZeroNumeric with as many zeros on the
-            left as are necessary for len(ZeroNumeric) == length.
+        Returns: A ZeroNumeric object with as many zeros on the left as
+            are necessary for len(self) == length.
 
         """
-        return '0' * max(length - len(self), 0) + self._value
+        return ZeroNumeric(
+            '0' * max(length - len(self), 0) + str(self._numeric)
+        )
+
+    @staticmethod
+    def split_zeros(value: str) -> tuple:
+        """
+        Breaks a zero-initial numeric string into two pieces, the
+        leading zeros, and the remaining numerals.
+
+        Args:
+            value: A numeric value stored as a string.
+
+        Returns: A tuple of zeros stored as string, and an integer or
+            float value representing the remainder of the numeric
+            value.
+
+        """
+        pieces = re.findall(r'(^0*)([1-9]+\d*\.*\d*)', value)[0]
+        return pieces[0], u.isnumericplus(
+                            pieces[1], '-convert', '-no_bool')
+
+    def do_op(self, op, other):
+        """
+        Runs a python operation on self._numeric.
+
+        Args:
+            op: A python operator object.
+            other: An object.
+
+        Returns: The result of the operation on self._value if other is
+            a string, or self._numeric.
+
+        """
+        if isinstance(str, other):
+            return op(self._value, other)
+        else:
+            return op(self._numeric, other)
+
+    def _mod(self, new_val: (int, float)):
+        """
+
+        Args:
+            new_val: An integer or float.
+
+        Returns: A new ZeroNumeric object with this ZeroNumeric's zeros
+            followed by new_val.
+
+        """
+        if new_val is 0:
+            return 0
+        else:
+            return ZeroNumeric(
+                self._zeros + str(new_val)
+            )
+
+    def __add__(self, other):
+        return self._mod(self.do_op(o.add, other))
 
     def __eq__(self, other):
-        return self._value == other
+        if isinstance(other, str):
+            return self._value == other
+        else:
+            return self._numeric == other
+
+    def __gt__(self, other):
+        return self._numeric > other
+
+    def __ge__(self, other):
+        return self._numeric >= other
+
+    def __le__(self, other):
+        return self._numeric <= other
 
     def __len__(self):
         return len(self._value)
 
+    def __lt__(self, other):
+        return self._numeric < other
+
+    def __mod__(self, other):
+        return self._mod(self.do_op(o.mod, other))
+
+    def __mul__(self, other):
+        return self._mod(self.do_op(o.mul, other))
+
     def __ne__(self, other):
-        return self._value != other
+        if isinstance(other, str):
+            return self._value != other
+        else:
+            return self._numeric != other
 
     def __repr__(self):
         return self._value
 
     def __str__(self):
-        return "'" + self._value
+        if pd.isna(self._value):
+            return str(self._value)
+        else:
+            return "'" + self._value
+
+    def __sub__(self, other):
+        return self._mod(self.do_op(o.sub, other))
+
+    def __truediv__(self, other):
+        return self._mod(self.do_op(o.truediv, other))
--- tests/element_test.py
+++ tests/element_test.py
@@ -1,3 +1,5 @@
+import operator as o
+
 import pytest
 
 import datagenius.element as e
@@ -5,14 +7,39 @@ import datagenius.element as e
 
 class TestZeroNumeric:
     def test_basics(self):
-        assert e.ZeroNumeric('00123') == '00123'
-        assert e.ZeroNumeric('00123') != '123'
-        assert str(e.ZeroNumeric('00123')) == "'00123"
+        z = e.ZeroNumeric('00124')
+        assert z.numeric == 124
+        assert z.value == '00124'
+        assert z.zeros == '00'
+        assert z == 124
+        assert z == '00124'
+        assert str(z) == "'00124"
+        x = z + 1
+        assert x == 125
+        assert isinstance(x, e.ZeroNumeric)
+        assert z - 1 == 123
+        assert z * 2 == 248
+        assert z / 2 == 62
+        assert z % 2 == 0
+        x = z - 124
+        assert x == 0
+        assert not isinstance(x, e.ZeroNumeric)
 
         with pytest.raises(
-                ValueError, match='numeric value. Invalid value=test'):
+                ValueError, match='or nan. Invalid value=test'):
             e.ZeroNumeric('test')
 
     def test_pad(self):
         assert e.ZeroNumeric('123').pad(3) == '123'
         assert e.ZeroNumeric('123').pad(6) == '000123'
+
+    def test_split_zeros(self):
+        assert e.ZeroNumeric.split_zeros('00123') == ('00', 123)
+        assert e.ZeroNumeric.split_zeros('00123.0') == ('00', 123.0)
+        assert e.ZeroNumeric.split_zeros('001203') == ('00', 1203)
+        assert e.ZeroNumeric.split_zeros('001203.0') == ('00', 1203.0)
+        assert e.ZeroNumeric.split_zeros('123') == ('', 123)
+
+    def test_do_op(self):
+        assert e.ZeroNumeric('00123').do_op(o.add, 22) == 145
+        assert e.ZeroNumeric('00123').do_op(o.sub, 22) == 101
