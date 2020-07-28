86ef79e767ee2eb42b951a89128037c7c66b50b6
christopher.larabee <christopher.larabee@mnhs.org>
2020-07-28 11:01:02 -0500

    Added SheetsAPI to the root-level imports.

--- datagenius/__init__.py
+++ datagenius/__init__.py
@@ -1,7 +1,7 @@
 from .element import ZeroNumeric
 from .genius import GeniusAccessor
 from .io import odbc
-from .io.text import get_output_template
+from .io.text import get_output_template, SheetsAPI
 from .lib import CleaningGuide, SupplementGuide, RedistributionGuide
 from .util import transmutation, nullable
 
@@ -9,7 +9,7 @@ from .util import transmutation, nullable
 __all__ = [
     'element', 'genius', 'util', 'CleaningGuide', 'SupplementGuide',
     'RedistributionGuide', 'ZeroNumeric', 'transmutation', 'nullable',
-    'get_output_template'
+    'get_output_template', 'SheetsAPI'
 ]
 
 
