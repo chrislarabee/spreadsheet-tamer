493b6ba601bcfb7d17ec391f5c2e881b0da99370
christopher.larabee <christopher.larabee@mnhs.org>
2020-08-13 21:09:26 -0500

    v.0.4.5: Added GeniusMetadata to top-level package __init__.

--- datagenius/__init__.py
+++ datagenius/__init__.py
@@ -1,5 +1,6 @@
 from .element import ZeroNumeric
 from .genius import GeniusAccessor
+from .metadata import GeniusMetadata
 from .io import odbc
 from .io.text import get_output_template, SheetsAPI
 from .lib import CleaningGuide, SupplementGuide, RedistributionGuide
@@ -9,8 +10,5 @@ from .util import transmutation, nullable
 __all__ = [
     'element', 'genius', 'util', 'CleaningGuide', 'SupplementGuide',
     'RedistributionGuide', 'ZeroNumeric', 'transmutation', 'nullable',
-    'get_output_template', 'SheetsAPI'
+    'get_output_template', 'SheetsAPI', 'GeniusMetadata'
 ]
-
-
-
