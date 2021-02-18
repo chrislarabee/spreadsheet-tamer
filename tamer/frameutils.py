6f2f4f9c968c5d4c39b2d41026447dd53f8d5f63
Chris <chris.larabee9@gmail.com>
2021-02-18 14:44:52 -0600

    Removed unused import.

--- tamer/frameutils.py
+++ tamer/frameutils.py
@@ -1,4 +1,3 @@
-from typing import Callable
 from typing import Callable, Any
 import pandas as pd
 
@@ -10,20 +9,20 @@ def multiapply(
     **column_func_pairs: Callable[[Any], Any]
 ) -> pd.DataFrame:
     """
-    Convenience function for applying a variety of single argument functions to 
-    various combinations of columns of a DataFrame. You can broadcast a single 
-    function over multiple columns, or select specific columns to apply a 
+    Convenience function for applying a variety of single argument functions to
+    various combinations of columns of a DataFrame. You can broadcast a single
+    function over multiple columns, or select specific columns to apply a
     specific function to, or both.
 
-    Note that if you apply a function to a column that contains nan values or 
-    mixed datatypes, you need to make sure your function can handle those 
-    scenarios. Wrapping your function in @nullable solves the nan problem at 
+    Note that if you apply a function to a column that contains nan values or
+    mixed datatypes, you need to make sure your function can handle those
+    scenarios. Wrapping your function in @nullable solves the nan problem at
     least.
 
     Args:
         df (pd.DataFrame): A DataFrame
         *columns (str): Arbitrary column labels from df.
-        func (Callable[[Any], Any], optional): A function to apply to the values 
+        func (Callable[[Any], Any], optional): A function to apply to the values
             in df[columns]. Defaults to None.
         **column_func_pairs [Callable[[Any], Any]]: Arbitrary column labels from
             df and the accompanying function that should be applied to them.
